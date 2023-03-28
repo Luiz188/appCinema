1c87d7ae51bbd45aeef92dbdc02b17c791b32f91
luiz188 <luck9365@gmail.com>
2023-03-27 21:28:40 -0300

    enviar para banco

--- app/Http/Controllers/cadastroFuncionario.php
+++ app/Http/Controllers/cadastroFuncionario.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\cadastroFuncionarioModel;
 
 class cadastroFuncionario extends Controller
 {
     public function buscaCadastroFuncionario(){
         return View('cadastroFuncionario');
+    }
+
+    public function cadastrarFuncionario (Request $request){
+        $dadosfuncionario = $request->validade(
+            [
+                'emailfun'=> 'string|required',
+                'nomefun'=> 'string|required',
+                'senhafun'=> 'string|required',
+                'whatsappfun'=> 'string|required',
+                'cpffun'=> 'string|required'
+            ]
+            );
+        cadastroFuncionarioModel::create($dadosfuncionario);
+        return Redirect::route('/home');
     }
 }
